e47549c56bcd3bbfb70c303a2b49f2968e8c3b78
KugelblitzTaco <johnkramer0101@gmail.com>
2022-07-31 04:30:30 -0600

    Update

--- ejemploScanner.java
+++ ejemploScanner.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ejemploScanner {
     public static void main(String[] args) {
+        //Inicializar valor en memoria.
+        Scanner spaceInMemory = new Scanner(System.in);
+        /*Se define el final del scanner, para que no siga leyendo datos. */
+        String myText = spaceInMemory.nextLine();
         System.out.println("Hey y'all");
+        /*Se imprime el valor leido*/
+        System.out.println(myText);
     }
 }
